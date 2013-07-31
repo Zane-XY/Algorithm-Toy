7a1d887d0b7ce0ab6ffd097b97801c74cf4fa38e
Zane.XY <zane.xy@live.com>
2013-07-31 13:51:05 +0800

    add ordering check

--- src/main/scala/xiaoye.wang/topcoder/AfraidOfEven.scala
+++ src/main/scala/xiaoye.wang/topcoder/AfraidOfEven.scala
@@ -1,28 +1,37 @@
 package xiaoye.wang.topcoder
 
+import scala.annotation.tailrec
+
 /**
  * Given an arithmetic progression T1 = a1, a2,...an (n >= 4, <= 1000), now divide every even value with 2,
  * until it's an odd, you'll get a T2, write a function that take a T2, return the minimal original T1
  * From TopCoder
  *
  * @author Zane.Wang
- * @note I think the equation can be optimized into more simpler form
+ *
  */
 object AfraidOfEven extends App {
 
   def restoreProgression(arr: Array[Int]): Array[Int] = {
     val (pre, odd, pos) = (arr(0), arr(1), arr(2))
     val (len, max) = (arr.length, arr.max)
+    val isAsc = arr.lastIndexOf(max) >= (len - 1).toFloat / 2
     val (rA, rB, rC) = ((0 to (max / pre)).toStream, (0 to (max / pos)).toStream, (0 to (max / odd)).toStream)
 
-    def equation(a: Int, b: Int, c: Int) = odd * (BigInt(2).pow(c + 1)) == (pre * (BigInt(2).pow(a)) + pos * (BigInt(2).pow(b)))
+    def pow2(n: Int): Int = n match {
+      case 0 => 1
+      case 1 => 2
+      case _ => pow2(n - 1) * 2
+    }
 
-    val (a, b, c) = (for (c <- rC; a <- rA; b <- rB) yield (a, b, c)).iterator.find(p => equation(p._1, p._2, p._3)).get
+    def diff(c: Int, a: Int) = odd * pow2(c) - pre * pow2(a)
+    def equation(a: Int, b: Int, c: Int) = odd * pow2(c + 1) == (pre * pow2(a) + pos * pow2(b))
+    def predicate(a: Int, b: Int, c: Int): Boolean = equation(a, b, c) && ((diff(c, a) >= 0) == isAsc)
 
-    val diff = (odd * (BigInt(2).pow(c)) - (pre * (BigInt(2).pow(a)))).toInt
+    val (a, _, c) = (for (_c <- rC; _a <- rA; _b <- rB) yield (_a, _b, _c)).iterator.find(x => predicate(x._1, x._2, x._3)).get
 
-    val head = arr(0) * BigInt(2).pow(a).toInt
-    (0 until len).map(_ * diff + head).toArray
+    val head = arr(0) * pow2(a)
+    (0 until len).map(_ * diff(c, a) + head).toArray
   }
 
   def test = {
@@ -32,6 +41,7 @@ object AfraidOfEven extends App {
     assert(restoreProgression(Array(9, 7, 5, 3, 1)).deep == Array(9, 7, 5, 3, 1).deep)
     assert(restoreProgression(Array(3, 7, 25, 9)).deep == Array(3, 14, 25, 36).deep)
     assert(restoreProgression(Array(1, 43, 54, 65)).deep == Array(32, 43, 54, 65).deep)
+    assert(restoreProgression(Array(1, 3, 1, 1)).deep == Array(4, 3, 2, 1).deep)
   }
 
   test
