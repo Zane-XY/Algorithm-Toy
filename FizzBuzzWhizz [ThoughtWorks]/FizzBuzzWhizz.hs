ee25f23c0acf5eb986f627f52a4236ea37499bc7
Zane.XY <zane.xy@live.com>
2014-05-01 22:42:28 +0800

    contrived shorten version

--- FizzBuzzWhizz [ThoughtWorks]/FizzBuzzWhizz.hs
+++ FizzBuzzWhizz [ThoughtWorks]/FizzBuzzWhizz.hs
@@ -6,11 +6,9 @@ main :: IO ()
 main = mapM_ (print . fizzBuzzWhizz (3,5,7)) [1 .. 100]
 
 fizzBuzzWhizz :: (Int, Int, Int) -> Int -> String
-fizzBuzzWhizz (a, b, c) i = snd $ fizz' a . to (a, "Fizz") . to (b, "Buzz") . to (c, "Whizz") $ (i, [])
-  where fizz' d (i', r)
-          | show d `isInfixOf` show i'  = (i', "Fizz")
-          | null r                      = (i', show i')
-          | otherwise                   = (i', r)
-        to (d, p) (i', r) | d == 0          = (i', r)
-                          | i' `mod` d == 0 = (i', p ++ r)
-                          | otherwise       = (i', r)
+fizzBuzzWhizz (a, b, c) i = case fbw [] of [] -> show i; x -> x
+  where fbw = foldl1' (.) $ countOff `map` [(a, []), (a, "Fizz"), (b, "Buzz"), (c, "Whizz")]
+        countOff (d, p) r
+          | show a `isInfixOf` show i = "Fizz"
+          | i `mod` d   == 0          = p ++ r
+          | otherwise                 = r
